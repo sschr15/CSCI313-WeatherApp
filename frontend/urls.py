f5b2c0021e679f4109ebe9cd52ef92d4d4396804
Griffin Winstead <grifwin03@gmail.com>
2024-04-29 17:18:30 -0500

    Added navbar, city view, radar view, and lunar view.

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-
+    path('city_view', city_view, name='city_view' ),
+    path('lunar_view', lunar_view, name='lunar_view' ),
+    path('radar_view', radar_view, name='radar_view' ),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def city_view(request):
+    return render(request, 'expanded_city_view.html')
+
+def lunar_view(request):
+    return render(request, 'expanded_lunar_view.html')
+
+def radar_view(request):
+    return render(request, 'expanded_radar_view.html')
