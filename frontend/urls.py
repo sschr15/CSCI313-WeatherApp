8325b81a6ec2340b6b98ce296433336c213f7379
Griffin Winstead <grifwin03@gmail.com>
2024-04-30 18:41:00 -0500

    Hourly/Weekly main page skeleton fully done

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('weekly_view', weekly_view, name='weekly_view'),
     path('city_view', city_view, name='city_view' ),
     path('lunar_view', lunar_view, name='lunar_view' ),
     path('radar_view', radar_view, name='radar_view' ),
